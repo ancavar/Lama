3f42692eca53f6d62d688c56d6a833f5b4f02ad4
ancavar <danaj2004@yandex.ru>
2026-02-10 11:02:52 +0300

    fix `unescape`

--- src/SM.ml
+++ src/SM.ml
@@ -208,6 +208,9 @@ module ByteCode = struct
         let rec iterate i =
           if i < n then
             match x.[i] with
+            | '"' -> 
+                Buffer.add_char buf '"';
+                iterate (i + 1)
             | '\\' -> (
                 if i + 1 >= n then
                   Buffer.add_char buf '\\'
@@ -221,12 +224,6 @@ module ByteCode = struct
                       iterate (i + 2)
                   | 'r' ->
                       Buffer.add_char buf '\r';
-                      iterate (i + 2)
-                  | '"' ->
-                      Buffer.add_char buf '"';
-                      iterate (i + 2)
-                  | '\\' ->
-                      Buffer.add_char buf '\\';
                       iterate (i + 2)
                   | _ ->
                       Buffer.add_char buf '\\';
